da4cca20d446c1ac000259446e780b7154c32576
Maximo Ramirez <a00839731@tec.mx>
2025-04-03 18:26:11 -0600

    Controller en funcionamiento, filtra de forma correcta por calificacion asi como por estado de adeudo

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -4,7 +4,25 @@ class StudentController {
 
     async getAllStudent() {
         const student = await studentService.getStudent();
-        return student
+        
+        const resultados = student.map(student => {
+            const resultado = {
+                Matricula: student.Matricula
+            };
+
+            
+            if (student.Adeudos) { // Tiene adeudos
+                if (student.Calificacion>=90){return {...resultado, Mensaje:"Tenias honores, pero tienes un adeudo"}}
+                else if (student.Calificacion < 70) {return {...resultado, Mensaje:"Expulsado"}}
+            } 
+            else if (student.Calificacion < 90 && student.Calificacion > 70) {return {...resultado, Mensaje: "Pasaste apenas"}} // Con o sin adeudos
+            else { // No tiene adeudos
+                if (student.Calificacion >= 90) {return {...resultado, Mensaje:"Te graduaste con honores"}}
+                else if (student.Calificacion < 70) {return {...resultado, Mensaje:"Como no tienes adeudo, tienes derecho a un examen de recuperacion"}}
+            }
+        });
+
+        return resultados;
     }
 }
 
--- src/db/studentDB.ts
+++ src/db/studentDB.ts
@@ -7,13 +7,13 @@ interface Student {
 
 class StudentService {
     private studentDB: Student[] = [
-        {Matricula: "A00839731" , Nombre: "Maximo Ramirez" , Adeudos: false , Calificacion: 9.5},
-        {Matricula: "A00839731" , Nombre: "Maximo Ramirez" , Adeudos: false , Calificacion: 9.5},
-        {Matricula: "A00839731" , Nombre: "Maximo Ramirez" , Adeudos: false , Calificacion: 9.5},
-        {Matricula: "A00839731" , Nombre: "Maximo Ramirez" , Adeudos: false , Calificacion: 9.5},
-        {Matricula: "A00839731" , Nombre: "Maximo Ramirez" , Adeudos: false , Calificacion: 9.5},
-        {Matricula: "A00839731" , Nombre: "Maximo Ramirez" , Adeudos: false , Calificacion: 9.5},
-        {Matricula: "A00839731" , Nombre: "Maximo Ramirez" , Adeudos: false , Calificacion: 9.5}
+        {Matricula: "A00839731" , Nombre: "Maximo Ramirez" , Adeudos: false , Calificacion: 95},
+        {Matricula: "A00562843" , Nombre: "Ana Sofía Mendoza" , Adeudos: true , Calificacion: 70},
+        {Matricula: "A00838972" , Nombre: "Fatima Castillo" , Adeudos: false , Calificacion: 85},
+        {Matricula: "A00839731" , Nombre: "Dana Torres" , Adeudos: false , Calificacion: 25},
+        {Matricula: "A00462181" , Nombre: "Valeria Jiménez" , Adeudos: false , Calificacion: 38},
+        {Matricula: "A00674521" , Nombre: "Daniel Sanchez" , Adeudos: true , Calificacion: 100},
+        {Matricula: "A00238496" , Nombre: "Sandino Leon" , Adeudos: true , Calificacion: 43}
     ];
 
     async getStudent():Promise<Student[]> {
